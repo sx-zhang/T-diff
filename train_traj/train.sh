68396bea68c034815537a758cb370697c14046d6
Tsunamiiii <65492893+TSUNAMIII-7@users.noreply.github.com>
2025-03-27 21:02:55 +0800

    Update train.sh

--- train_traj/train.sh
+++ train_traj/train.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=diffusion_traj
 export CUDA_VISIBLE_DEVICES=0
 
-conda activate diff
+conda activate diff_train
 
 python train.py --config-dir=. --config-name=train_diffusion_traj_gibson.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_train_traj_diff_gibson'
